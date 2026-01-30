6f4f03237b31e5ef4d05f8470a77663a0532fad4
JD-stack <deokule.jay.2004@gmail.com>
2026-01-30 16:11:23 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,9 +57,9 @@ export function Home() {
     backgroundImage: `
       linear-gradient(
         to bottom right,
-        rgba(0,0 , 0, 0.65),
-        rgba(0, 0, 0, 0.65),
-        rgba(0, 0, 0, 0.65)
+        rgba(0,0 , 0, 0),
+        rgba(0, 0, 0, 0),
+        rgba(0, 0, 0, 0)
       ),
       url("https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&w=1600&q=80")
     `,
