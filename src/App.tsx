435d769c4a9ae15d98e1c2cb9b72006dfc7d73a8
JD-stack <deokule.jay.2004@gmail.com>
2026-02-15 18:55:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from './components/ui/sonner';
-import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
-import { Services } from './pages/Services';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import { Login } from './pages/Login';
-import { SignUp } from './pages/SignUp';
-import { NavBar } from './components/NavBar';
-import Blog from './pages/Blog';
-import { AdminDashboard } from './pages/AdminDashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { Services } from "./pages/Services";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+import { NavBar } from "./components/NavBar";
+import Blog from "./pages/Blog";
+import { AdminDashboard } from "./pages/AdminDashboard";
+
+/* NEW BLOG SYSTEM */
+import Posts from "./pages/Posts";
+import PostView from "./pages/PostView";
+import PostEditor from "./pages/PostEditor";
+import AdminRoute from "./components/AdminRoute";
 
 export default function App() {
   return (
@@ -26,11 +32,25 @@ export default function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blog" element={<Blog />} />
 
+            {/* New Blog System */}
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/:id" element={<PostView />} />
+
+            {/* Admin Post Editor */}
+            <Route
+              path="/admin/posts/new"
+              element={
+                <AdminRoute>
+                  <PostEditor />
+                </AdminRoute>
+              }
+            />
+
             {/* Auth pages */}
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
 
-            {/* Admin page */}
+            {/* Admin dashboard */}
             <Route path="/admin" element={<AdminDashboard />} />
           </Routes>
         </main>
@@ -39,18 +59,5 @@ export default function App() {
         <Toaster />
       </div>
     </Router>
-    <Route path="/posts" element={<Posts />} />
-<Route path="/posts/:id" element={<PostView />} />
-
-<Route
-  path="/admin/posts/new"
-  element={
-    <AdminRoute>
-      <PostEditor />
-    </AdminRoute>
-  }
-/>
-
   );
 }
-
