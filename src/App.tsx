ff9ca5466309b517f37a6eba1c88020577c5c259
JD-stack <deokule.jay.2004@gmail.com>
2026-02-16 19:12:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
       <div className="flex flex-col min-h-screen">
         <NavBar />
 
-        <main className="flex-grow min-h-screen overflow-visible pt-16">
+        <main className="flex-grow">
           <Routes>
             {/* Public Pages */}
             <Route path="/" element={<Home />} />
